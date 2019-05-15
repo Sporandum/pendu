b3afe54b7027321032bba0e438c80557268ce0aa
sporandum <sporandum@gmail.com>
2019-05-15 17:05:14 +0200

    fonction créer une div pour chaque lettre

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function foo(w) {
     let nbWords = w.length,
         i = parseInt(Math.random() * (0 - nbWords) + nbWords),
         newWord = w[i];
-        
+
     if (word === null || word !== newWord) {
         return word = newWord;
     } else if (word !== null || word === newWord) {
@@ -22,8 +22,23 @@ function foo(w) {
     }
 }
 
+foo(words);
 // séparer chaque lettre du mot individuellement
 
+let letters = word.split('');
+
 // créer une div pour chaque lettre du mot avec la classe .letter
+
+function bar(letters) {
+    let l = letters.length,
+        lettersElts = document.createElement('div'),
+        i;
+
+        for (i = 0; i < l; i++) {
+            let letterElt = document.createElement('div');
+            lettersElts.appendChild(letterElt)
+        }
+        return lettersElts;
+}
 
 // insérer les divs dans le DOM elt #word-to-find
